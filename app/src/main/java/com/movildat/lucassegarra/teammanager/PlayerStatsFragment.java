77217638c02f001023dbb54f48c94f04a97e05b9
pilotAlpal <lucas.segarra.fernandez@estumail.ucm.es>
2016-07-26 10:57:40 +0200

    OptionsFragment.hideNextMatchInfo:No esconde el RecyclerView de NextMatchFragment una vez lo despliega en InfoFragment.
    Actualizadas algunas de las vistas ya existentes y creación de nuevos fragmentos para añadir las funcionalidades de configurar el pérfil propio o contactar con uno ajeno.

--- app/src/main/java/com/movildat/lucassegarra/teammanager/PlayerStatsFragment.java
+++ app/src/main/java/com/movildat/lucassegarra/teammanager/PlayerStatsFragment.java
@@ -12,7 +12,7 @@ import android.widget.Spinner;
  * Created by lucas.segarra on 15/07/2016.
  */
 public class PlayerStatsFragment extends Fragment {
-    private Spinner posChooser;
+
 
     @Override public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -21,11 +21,7 @@ public class PlayerStatsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String[] pos= getResources().getStringArray(R.array.posiciones);
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,pos);
-        View v=inflater.inflate(R.layout.player_stats_view,container,false);
-        posChooser=(Spinner) v.findViewById(R.id.sp_pos);
-        posChooser.setAdapter(adapter);
-        return v;
+        return inflater.inflate(R.layout.player_stats_view,container,false);
+
     }
 }
