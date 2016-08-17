d00b88fa2d774d76d9c1db9aa5803e036a0b23ae
pilotAlpal <lucas.segarra.fernandez@estumail.ucm.es>
2016-08-17 23:15:56 +0200

    solucionnado pequeño fallo menuactivity

--- app/src/main/java/com/movildat/lucassegarra/teammanager/view/MenuActivity.java
+++ app/src/main/java/com/movildat/lucassegarra/teammanager/view/MenuActivity.java
@@ -40,7 +40,7 @@ public class MenuActivity extends ViewActivity {
     }
 
     public void statsJugador(View view){
-        PlayerStatsFragment pSf=PlayerStatsFragment.newInstance(myController.getMyplayerStats());
+        PlayerStatsFragment pSf=PlayerStatsFragment.newInstance(myController.getMyplayerStats(),myController);
         getFragmentManager().beginTransaction().replace(R.id.f_info,pSf).commit();
     }
     public void statsEquipo(View view){
